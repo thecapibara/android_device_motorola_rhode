eeff743628212a2de66697ea4d523806d0a10cfe
thecapibara <49768197+thecapibara@users.noreply.github.com>
2025-05-25 22:00:46 +0300

    TEST (lineage -> 2by2) + filename

--- rhode.mk
+++ rhode.mk
@@ -10,8 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/rhode/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common 2by2 stuff.
+$(call inherit-product, vendor/2by2/config/common_full_phone.mk)
 WITH_GMS := true
 TARGET_USES_PICO_GAPPS := true
 TARGET_INCLUDE_EXTRA_APPS := false
@@ -19,7 +19,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 CUSTOM_MAINTAINER := JustGL
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_rhode
+PRODUCT_NAME := rhode
 PRODUCT_DEVICE := rhode
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g52
