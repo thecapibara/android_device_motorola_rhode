161cf4565ffa7d251be15486080ffe8c451b963c
George <49768197+thecapibara@users.noreply.github.com>
2025-06-25 14:03:44 +0300

    Attempt to fix some stuff

--- lineage_rhode.mk
+++ lineage_rhode.mk
@@ -12,6 +12,7 @@ $(call inherit-product, device/motorola/rhode/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_DISABLE_EPPE := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_rhode
