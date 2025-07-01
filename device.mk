0ced659f4a6624a5d91b98a0cc9a35c2d38669d1
George <49768197+thecapibara@users.noreply.github.com>
2025-07-01 08:51:30 +0300

    Added MotoCam support

--- device.mk
+++ device.mk
@@ -60,3 +60,9 @@ PRODUCT_COPY_FILES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/rhode/rhode-vendor.mk)
+
+#MotoCamera 4
+TARGET_MOTCAMERA4 := devon
+TARGET_USES_MOTCAMERA4 := true
+
+$(call inherit-product, vendor/motorola/MotCamera4-bengal/motcamera4.mk)
